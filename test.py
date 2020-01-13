a883fd9b565888a3548bf10a0b57c37f9e1a8765
jax79sg <jax79sg@yahoo.com.sg>
2020-01-13 15:02:24 +0800

    update

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ class TestModels(unittest.TestCase):
     def test_sqlsaver1createreport(self):
         import os
         os.system('rm /mnt/shareddata/testrun.txt')
+        os.system('mkdir /mnt')
+        os.system('mkdir /mnt/sharedata')
         os.system('touch /mnt/shareddata/testrun.txt')
 
 
