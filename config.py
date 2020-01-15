80b691d5e493b3be419f3b81a26e0133fdbf8a1d
jax79sg <jax79sg@yahoo.com.sg>
2020-01-15 09:43:59 +0800

    update

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class MonitorConfig():
     
-    DATAPATH="/shareddata/new"
-    PROCESSINGPATH="/shareddata/processing"
-    SUCCESSPATH="/shareddata/success"
-    FAILPATH="/shareddata/fail"
+    DATAPATH="/shareddata/new/"
+    PROCESSINGPATH="/shareddata/processing/"
+    SUCCESSPATH="/shareddata/success/"
+    FAILPATH="/shareddata/fail/"
     CREATEJOB_URL="http://jobsvc:9891/create_job"
 
