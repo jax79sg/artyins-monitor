2c8c48780c2f9205ad3b07549dcd91c552534bb2
jax79sg <jax79sg@yahoo.com.sg>
2020-01-16 11:08:48 +0800

    update

--- monitor.py
+++ monitor.py
@@ -2,29 +2,31 @@ import sys
 import time
 import logging
 from config import MonitorConfig
+import requests
+import shutil
 import glob
 
 config=MonitorConfig()
 path = config.DATAPATH
 
-logging.basicConfig(level=logging.INFO,handlers=[
-        logging.FileHandler("{0}/{1}.log".format("/logs", "eventhandler")),
+logging.basicConfig(level=logging.DEBUG,handlers=[
+        logging.FileHandler("{0}/{1}.log".format("/logs", "monitor")),
         logging.StreamHandler()
         ],format='%(asctime)s - %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
 
-def markprocessing(self,filename):
+def markprocessing(filename):
         logging.info("Moving %s to PROCESSING", filename)
         shutil.move(config.DATAPATH+filename,config.PROCESSINGPATH+filename)        
 
-def marksuccess(self, filename):
+def marksuccess(filename):
         logging.info("Moving %s to SUCCESS", filename)
         shutil.move(config.PROCESSINGPATH+filename, config.SUCCESSPATH+filename)
 
-def markfail(self, filename):
+def markfail( filename):
         logging.info("Moving $s to FAILED", filename)
         shutil.move(config.PROCESSINGPATH+filename, config.FAILPATH+filename)
 
-def create_job(self,filename):
+def create_job(filename):
         DATA=[{"filename":filename}]
         logging.info("Sending a new job with %s to %s", filename, config.CREATEJOB_URL)
         r = requests.post(url = config.CREATEJOB_URL, json  = DATA)
@@ -48,7 +50,8 @@ if __name__ == "__main__":
             logging.debug("Waiting for next interval of %s secs", config.CHECK_INTERVAL)
             time.sleep(int(config.CHECK_INTERVAL))
             import glob
-            listoffiles = glob.glob(config.DATAPATH+"*")
+            listoffiles = glob.glob(config.DATAPATH+"*.*")
+            logging.debug("Globbing %s yields %s", config.DATAPATH+"*.*", listoffiles)
             for filepath in listoffiles:
                 logging.debug("Processing %s", filepath)
                 filename=filepath.split("/")[-1]
